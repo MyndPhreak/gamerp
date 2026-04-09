d93acf74015eaa36dd39865e369e3b3853eb2d7b
Dylan Roberson <myndphreak1@gmail.com>
2026-04-08 22:58:24 -0400

    feat: add ClothingList field to PlayerData for outfit persistence

--- code/Database/PlayerData.cs
+++ code/Database/PlayerData.cs
@@ -9,5 +9,9 @@ public class PlayerData
 	public string Name { get; set; }
 	public int Money { get; set; }
 	public string JobTitle { get; set; }
+	/// <summary>
+	/// JSON array of equipped clothing resource paths
+	/// </summary>
+	public string ClothingList { get; set; } = "[]";
 	public DateTime LastSeen { get; set; }
 }
